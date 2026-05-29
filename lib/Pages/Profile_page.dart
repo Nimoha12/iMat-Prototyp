f4476b89309f860a82837f4b45bd9875cdd1515b
VeraTibu2 <tiburcia@chalmers.se>
2026-05-29 09:26:52 +0200

    new profilepage

--- lib/Pages/Profile_page.dart
+++ lib/Pages/Profile_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:imat_repo/Widgets/Navigation/navbar.dart';
-import 'package:imat_repo/Widgets/Profile_Parts/Costumer_card.dart';
 import 'package:imat_repo/Widgets/Profile_Parts/Header/LogoutButton%20.dart';
-import 'package:imat_repo/Widgets/Profile_Parts/Payment_Card.dart';
+import 'package:imat_repo/Widgets/Profile_Parts/Header/CloseProfile_Button.dart';
+import 'package:imat_repo/Widgets/Profile_Parts/profile_constants.dart';
+import 'package:imat_repo/Widgets/Profile_Parts/profile_editable_field.dart';
+import 'package:imat_repo/Widgets/Profile_Parts/profile_module_tabs.dart';
+import 'package:imat_repo/model/imat_data_handler.dart';
+import 'package:provider/provider.dart';
+
+const String accountTabTitle = 'Kontoinformation';
+const String addressTabTitle = 'Adress';
+const String paymentTabTitle = 'Betalningsinformation';
+const String accountHelperText =
+    'Uppdatera namn, kontaktuppgifter och inloggningsnamn.';
+const String addressHelperText =
+    'Spara den adress som används som förval vid leverans.';
+const String paymentHelperText =
+    'Hantera sparade uppgifter för kort och faktura.';
 
 class ProfilePage extends StatefulWidget {
   static const routeName = '/profile';
@@ -14,149 +28,415 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  bool _editing = false;
+  bool _initialized = false;
+  bool _syncedLoadedProfileData = false;
 
-  final GlobalKey<CustomerCardState> _customerKey = GlobalKey();
-  final GlobalKey<PaymentCardState> _paymentKey = GlobalKey();
+  late final TextEditingController _firstNameController;
+  late final TextEditingController _lastNameController;
+  late final TextEditingController _emailController;
+  late final TextEditingController _mobilePhoneController;
+  late final TextEditingController _phoneController;
+  late final TextEditingController _userNameController;
 
-  void _toggleEditing() {
-    if (_editing) {
-      _customerKey.currentState?.saveCustomer();
-      _paymentKey.currentState?.saveCard();
+  late final TextEditingController _addressController;
+  late final TextEditingController _postCodeController;
+  late final TextEditingController _cityController;
+
+  late final TextEditingController _cardHolderController;
+  late final TextEditingController _cardTypeController;
+  late final TextEditingController _cardNumberController;
+  late final TextEditingController _cardCvvController;
+  late final TextEditingController _cardMonthController;
+  late final TextEditingController _cardYearController;
+  late final TextEditingController _invoiceEmailController;
+  late final TextEditingController _invoicePhoneController;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (_initialized) {
+      return;
     }
 
-    setState(() {
-      _editing = !_editing;
-    });
+    final handler = context.read<ImatDataHandler>();
+    final customer = handler.getCustomer();
+    final card = handler.getCreditCard();
+    final user = handler.getUser();
+
+    _firstNameController = TextEditingController(text: customer.firstName);
+    _lastNameController = TextEditingController(text: customer.lastName);
+    _emailController = TextEditingController(text: customer.email);
+    _mobilePhoneController = TextEditingController(
+      text: customer.mobilePhoneNumber,
+    );
+    _phoneController = TextEditingController(text: customer.phoneNumber);
+    _userNameController = TextEditingController(text: user.userName);
+
+    _addressController = TextEditingController(text: customer.address);
+    _postCodeController = TextEditingController(text: customer.postCode);
+    _cityController = TextEditingController(text: customer.postAddress);
+
+    _cardHolderController = TextEditingController(text: card.holdersName);
+    _cardTypeController = TextEditingController(text: card.cardType);
+    _cardNumberController = TextEditingController(text: card.cardNumber);
+    _cardCvvController = TextEditingController(
+      text: card.verificationCode.toString(),
+    );
+    _cardMonthController = TextEditingController(
+      text: card.validMonth.toString(),
+    );
+    _cardYearController = TextEditingController(
+      text: card.validYear.toString(),
+    );
+    _invoiceEmailController = TextEditingController(text: customer.email);
+    _invoicePhoneController = TextEditingController(
+      text: customer.mobilePhoneNumber,
+    );
+
+    _initialized = true;
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _mobilePhoneController.dispose();
+    _phoneController.dispose();
+    _userNameController.dispose();
+    _addressController.dispose();
+    _postCodeController.dispose();
+    _cityController.dispose();
+    _cardHolderController.dispose();
+    _cardTypeController.dispose();
+    _cardNumberController.dispose();
+    _cardCvvController.dispose();
+    _cardMonthController.dispose();
+    _cardYearController.dispose();
+    _invoiceEmailController.dispose();
+    _invoicePhoneController.dispose();
+    super.dispose();
+  }
+
+  void _saveCustomer({
+    String? firstName,
+    String? lastName,
+    String? email,
+    String? mobilePhone,
+    String? phone,
+    String? address,
+    String? postCode,
+    String? city,
+  }) {
+    final handler = context.read<ImatDataHandler>();
+    final customer = handler.getCustomer();
+
+    customer.firstName = firstName ?? customer.firstName;
+    customer.lastName = lastName ?? customer.lastName;
+    customer.email = email ?? customer.email;
+    customer.mobilePhoneNumber = mobilePhone ?? customer.mobilePhoneNumber;
+    customer.phoneNumber = phone ?? customer.phoneNumber;
+    customer.address = address ?? customer.address;
+    customer.postCode = postCode ?? customer.postCode;
+    customer.postAddress = city ?? customer.postAddress;
+
+    handler.setCustomer(customer);
+  }
+
+  void _saveUserName(String userName) {
+    final handler = context.read<ImatDataHandler>();
+    final user = handler.getUser();
+    user.userName = userName;
+    handler.setUser(user);
+  }
+
+  void _saveCard({
+    String? holder,
+    String? type,
+    String? number,
+    String? cvv,
+    String? month,
+    String? year,
+  }) {
+    final handler = context.read<ImatDataHandler>();
+    final card = handler.getCreditCard();
+
+    card.holdersName = holder ?? card.holdersName;
+    card.cardType = type ?? card.cardType;
+    card.cardNumber = number ?? card.cardNumber;
+    card.verificationCode = int.tryParse(cvv ?? '') ?? card.verificationCode;
+    card.validMonth = int.tryParse(month ?? '') ?? card.validMonth;
+    card.validYear = int.tryParse(year ?? '') ?? card.validYear;
+
+    handler.setCreditCard(card);
+  }
+
+  void _syncLoadedProfileData(ImatDataHandler handler) {
+    if (!_initialized || _syncedLoadedProfileData) {
+      return;
+    }
+
+    final customer = handler.getCustomer();
+    final card = handler.getCreditCard();
+    final user = handler.getUser();
+    final hasLoadedProfileData =
+        customer.firstName.isNotEmpty ||
+        customer.lastName.isNotEmpty ||
+        customer.email.isNotEmpty ||
+        customer.address.isNotEmpty ||
+        card.cardNumber.isNotEmpty ||
+        user.userName.isNotEmpty;
+
+    if (!hasLoadedProfileData) {
+      return;
+    }
+
+    _firstNameController.text = customer.firstName;
+    _lastNameController.text = customer.lastName;
+    _emailController.text = customer.email;
+    _mobilePhoneController.text = customer.mobilePhoneNumber;
+    _phoneController.text = customer.phoneNumber;
+    _userNameController.text = user.userName;
+
+    _addressController.text = customer.address;
+    _postCodeController.text = customer.postCode;
+    _cityController.text = customer.postAddress;
+
+    _cardHolderController.text = card.holdersName;
+    _cardTypeController.text = card.cardType;
+    _cardNumberController.text = card.cardNumber;
+    _cardCvvController.text = card.verificationCode.toString();
+    _cardMonthController.text = card.validMonth.toString();
+    _cardYearController.text = card.validYear.toString();
+    _invoiceEmailController.text = customer.email;
+    _invoicePhoneController.text = customer.mobilePhoneNumber;
+
+    _syncedLoadedProfileData = true;
   }
 
   @override
   Widget build(BuildContext context) {
+    _syncLoadedProfileData(context.watch<ImatDataHandler>());
+
     return Scaffold(
-      backgroundColor: const Color(0xFFF6F3ED),
+      backgroundColor: profilePageBackground,
+      appBar: const IMatNavbar(activePage: NavbarPage.profile),
+      body: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            final horizontalPadding = constraints.maxWidth < 900 ? 20.0 : 40.0;
 
-      appBar: const IMatNavbar(
-        activePage: NavbarPage.profile,
-      ),
-
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(40),
-
-          child: Column(
-            children: [
-             //header
-
-              Row(
-                mainAxisAlignment:
-                    MainAxisAlignment.spaceBetween,
-
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-
-                        icon: const Icon(
-                          Icons.close,
-                          size: 42,
-                        ),
-                      ),
-
-                      const SizedBox(width: 36),
-
-                      const Text(
-                        "Min profil",
-                        style: TextStyle(
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  const LogoutButton(),
-                ],
-              ),
-
-              const SizedBox(height: 40),
-
-              //content
-
-              IntrinsicHeight(
-                child: Row(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.stretch,
-
-                  children: [
-                    //customer
-
-                    Expanded(
-                      child: CustomerCard(
-                        key: _customerKey,
-                        isEditing: _editing,
-                      ),
+            return Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(
+                      horizontalPadding,
+                      96,
+                      horizontalPadding,
+                      34,
                     ),
-
-                    const SizedBox(width: 24),
-
-                    //payment column
-
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment:
-                            CrossAxisAlignment.stretch,
-
-                        children: [
-
-                          const SizedBox(height: 24),
-
-                          Expanded(
-                            child: PaymentCard(
-                              key: _paymentKey,
-                              isEditing: _editing,
+                    child: Center(
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxWidth: profileMaxWidth,
+                        ),
+                        child: ProfileModuleTabs(
+                          tabs: [
+                            ProfileModuleTab(
+                              title: accountTabTitle,
+                              icon: Icons.person,
+                              child: _buildAccountModule(),
                             ),
-                          ),
-                          const SizedBox(height: 24),
-                          Align(
-  alignment: Alignment.center,
-  child: SizedBox(
-    width: 460, // adjust as needed
-    height: 84,
-    child: ElevatedButton.icon(
-      onPressed: _toggleEditing,
-      icon: Icon(
-        _editing ? Icons.save : Icons.edit,
-        size: 28,
-      ),
-      label: Text(
-        _editing ? 'Spara' : 'Redigera',
-      ),
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF3B7B67),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
-        ),
-        textStyle: const TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.w800,
-        ),
-      ),
-    ),
-  ),
-),
-                        ],
+                            ProfileModuleTab(
+                              title: addressTabTitle,
+                              icon: Icons.home,
+                              child: _buildAddressModule(),
+                            ),
+                            ProfileModuleTab(
+                              title: paymentTabTitle,
+                              icon: Icons.credit_card,
+                              child: _buildPaymentModule(),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
+                const Positioned(top: 24, left: 24, child: LogoutButton()),
+                const Positioned(
+                  top: 12,
+                  right: 16,
+                  child: CloseProfileButton(),
+                ),
+              ],
+            );
+          },
         ),
       ),
+    );
+  }
+
+  Widget _buildAccountModule() {
+    return ProfileModuleSurface(
+      title: accountTabTitle,
+      helperText: accountHelperText,
+      icon: Icons.person,
+      groups: [
+        ProfileFieldGroup(
+          fields: [
+            ProfileEditableField(
+              label: 'Förnamn',
+              controller: _firstNameController,
+              onSave: (value) => _saveCustomer(firstName: value),
+            ),
+            ProfileEditableField(
+              label: 'E-post',
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              onSave: (value) {
+                _invoiceEmailController.text = value;
+                _saveCustomer(email: value);
+              },
+            ),
+            ProfileEditableField(
+              label: 'Efternamn',
+              controller: _lastNameController,
+              onSave: (value) => _saveCustomer(lastName: value),
+            ),
+
+            ProfileEditableField(
+              label: 'Mobilnummer',
+              controller: _mobilePhoneController,
+              keyboardType: TextInputType.phone,
+              onSave: (value) {
+                _invoicePhoneController.text = value;
+                _saveCustomer(mobilePhone: value);
+              },
+            ),
+            ProfileEditableField(
+              label: 'Telefon',
+              controller: _phoneController,
+              keyboardType: TextInputType.phone,
+              onSave: (value) => _saveCustomer(phone: value),
+            ),
+            ProfileEditableField(
+              label: 'Användarnamn',
+              controller: _userNameController,
+              onSave: _saveUserName,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildAddressModule() {
+    return ProfileModuleSurface(
+      title: addressTabTitle,
+      helperText: addressHelperText,
+      icon: Icons.home,
+      groups: [
+        ProfileFieldGroup(
+          fields: [
+            ProfileEditableField(
+              label: 'Gatuadress',
+              controller: _addressController,
+              onSave: (value) => _saveCustomer(address: value),
+            ),
+            ProfileEditableField(
+              label: 'Postnummer',
+              controller: _postCodeController,
+              keyboardType: TextInputType.number,
+              maxLength: 6,
+              onSave: (value) => _saveCustomer(postCode: value),
+            ),
+            ProfileEditableField(
+              label: 'Ort',
+              controller: _cityController,
+              onSave: (value) => _saveCustomer(city: value),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildPaymentModule() {
+    return ProfileModuleSurface(
+      title: paymentTabTitle,
+      helperText: paymentHelperText,
+      icon: Icons.credit_card,
+      groups: [
+        ProfileFieldGroup(
+          title: 'Betalkort',
+          fields: [
+            ProfileEditableField(
+              label: 'Kortinnehavare',
+              controller: _cardHolderController,
+              onSave: (value) => _saveCard(holder: value),
+            ),
+            ProfileEditableField(
+              label: 'Korttyp',
+              controller: _cardTypeController,
+              onSave: (value) => _saveCard(type: value),
+            ),
+            ProfileEditableField(
+              label: 'Kortnummer',
+              controller: _cardNumberController,
+              keyboardType: TextInputType.number,
+              maxLength: 19,
+              onSave: (value) => _saveCard(number: value),
+            ),
+            ProfileEditableField(
+              label: 'CVV',
+              controller: _cardCvvController,
+              keyboardType: TextInputType.number,
+              maxLength: 4,
+              onSave: (value) => _saveCard(cvv: value),
+            ),
+            ProfileEditableField(
+              label: 'Månad',
+              controller: _cardMonthController,
+              keyboardType: TextInputType.number,
+              maxLength: 2,
+              onSave: (value) => _saveCard(month: value),
+            ),
+            ProfileEditableField(
+              label: 'År',
+              controller: _cardYearController,
+              keyboardType: TextInputType.number,
+              maxLength: 4,
+              onSave: (value) => _saveCard(year: value),
+            ),
+          ],
+        ),
+        ProfileFieldGroup(
+          title: 'Faktura',
+          fields: [
+            ProfileEditableField(
+              label: 'Faktura e-post',
+              controller: _invoiceEmailController,
+              keyboardType: TextInputType.emailAddress,
+              onSave: (value) {
+                _emailController.text = value;
+                _saveCustomer(email: value);
+              },
+            ),
+            ProfileEditableField(
+              label: 'Faktura telefon',
+              controller: _invoicePhoneController,
+              keyboardType: TextInputType.phone,
+              onSave: (value) {
+                _mobilePhoneController.text = value;
+                _saveCustomer(mobilePhone: value);
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
---
+++ lib/Widgets/Profile_Parts/profile_editable_field.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:imat_repo/Widgets/Profile_Parts/profile_constants.dart';
+
+const String profileSaveTooltip = 'Spara ändring';
+const String profileEditHint = 'Klicka för att redigera';
+const String profileSavedMessage = 'Sparat';
+
+class ProfileEditableField extends StatefulWidget {
+  final String label;
+  final TextEditingController controller;
+  final ValueChanged<String> onSave;
+  final TextInputType? keyboardType;
+  final int? maxLength;
+
+  const ProfileEditableField({
+    super.key,
+    required this.label,
+    required this.controller,
+    required this.onSave,
+    this.keyboardType,
+    this.maxLength,
+  });
+
+  @override
+  State<ProfileEditableField> createState() => _ProfileEditableFieldState();
+}
+
+class _ProfileEditableFieldState extends State<ProfileEditableField> {
+  final FocusNode _focusNode = FocusNode();
+  bool _editing = false;
+  late String _savedValue;
+
+  @override
+  void initState() {
+    super.initState();
+    _savedValue = widget.controller.text;
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void didUpdateWidget(covariant ProfileEditableField oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.controller != widget.controller) {
+      _savedValue = widget.controller.text;
+    }
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    if (!_focusNode.hasFocus && _editing) {
+      _save();
+    }
+  }
+
+  void _startEditing() {
+    if (_editing) {
+      return;
+    }
+
+    setState(() {
+      _editing = true;
+    });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) {
+        return;
+      }
+      _focusNode.requestFocus();
+      widget.controller.selection = TextSelection(
+        baseOffset: 0,
+        extentOffset: widget.controller.text.length,
+      );
+    });
+  }
+
+  void _save() {
+    final nextValue = widget.controller.text.trim();
+    if (widget.controller.text != nextValue) {
+      widget.controller.text = nextValue;
+      widget.controller.selection = TextSelection.collapsed(
+        offset: nextValue.length,
+      );
+    }
+
+    if (nextValue != _savedValue) {
+      widget.onSave(nextValue);
+      _savedValue = nextValue;
+      ScaffoldMessenger.of(context)
+        ..hideCurrentSnackBar()
+        ..showSnackBar(
+          SnackBar(
+            content: Text('${widget.label}: $profileSavedMessage'),
+            duration: const Duration(milliseconds: 1200),
+          ),
+        );
+    }
+
+    if (mounted) {
+      setState(() {
+        _editing = false;
+      });
+    }
+  }
+
+  void _cancel() {
+    widget.controller.text = _savedValue;
+    widget.controller.selection = TextSelection.collapsed(
+      offset: _savedValue.length,
+    );
+    setState(() {
+      _editing = false;
+    });
+    _focusNode.unfocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      textField: true,
+      label: '${widget.label}. $profileEditHint',
+      child: SizedBox(
+        width: profileFieldWidth,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(widget.label, style: profileFieldLabelStyle),
+            const SizedBox(height: 8),
+            Shortcuts(
+              shortcuts: const <ShortcutActivator, Intent>{
+                SingleActivator(LogicalKeyboardKey.escape): _CancelEditIntent(),
+              },
+              child: Actions(
+                actions: <Type, Action<Intent>>{
+                  _CancelEditIntent: CallbackAction<_CancelEditIntent>(
+                    onInvoke: (_) {
+                      _cancel();
+                      return null;
+                    },
+                  ),
+                },
+                child: TextField(
+                  controller: widget.controller,
+                  focusNode: _focusNode,
+                  readOnly: !_editing,
+                  keyboardType: widget.keyboardType,
+                  maxLength: widget.maxLength,
+                  textInputAction: TextInputAction.done,
+                  onTap: _startEditing,
+                  onSubmitted: (_) => _save(),
+                  style: profileFieldTextStyle,
+                  decoration: InputDecoration(
+                    counterText: '',
+                    filled: true,
+                    fillColor: _editing
+                        ? profileSurfaceColor
+                        : profilePrimaryLightColor.withValues(alpha: 0.55),
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 15,
+                    ),
+                    suffixIcon: IconButton(
+                      tooltip: _editing ? profileSaveTooltip : profileEditHint,
+                      onPressed: _editing ? _save : _startEditing,
+                      icon: Icon(_editing ? Icons.check : Icons.edit),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(profileCardRadius),
+                      borderSide: const BorderSide(
+                        color: profileBorderColor,
+                        width: 1.5,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(profileCardRadius),
+                      borderSide: const BorderSide(
+                        color: profilePrimaryColor,
+                        width: 3,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CancelEditIntent extends Intent {
+  const _CancelEditIntent();
+}
